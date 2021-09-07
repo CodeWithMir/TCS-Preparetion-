fd581eeafc69aebfb09269f7fe7ac477e132fe7a
MIR JASIMUDDIN <89864349+CodeWithMir@users.noreply.github.com>
2021-09-07 16:53:51 +0530

    Update Program of Q1.c

--- Program of Q1.c
+++ Program of Q1.c
@@ -1,49 +1,48 @@
 /******************************************************************************
-    Q.given a pattern is [1 1 2 3 4 9 8 27...........]
+    Q1.given a pattern is [1 1 2 3 4 9 8 27...........]
     write a program so that anyone can find up to the nth tearm.......
     
                     '''''WELCOME TO CODE WITH MIR'''''
 
 *******************************************************************************/
+#include<stdio.h>
+#include<math.h>
+    
+int three(int n)
+{
+    int x;
+    //n-1 because powers start from 0 not 1
+    x = pow(3,n-1);
+    printf("Expected Output – %d",x);
+}
 
-#include <stdio.h>
+int two(int n)
+{
+    int x;
+    //n-1 because powers start from 0 not 1
+    x = pow(2,n-1);
+    printf("Expected Output – %d",x);
+}
 
 int main()
 {
-    int n,a,b,i;
+    int n;
     printf("\n                                   WELCOME TO CODE WITH MIR");
-    printf("\n enter the number n :");
+    printf("\nInput- ");
     scanf("%d",&n);
-    n--;
-        for(i=1;i<=n;i++)
-        
-            {
-                if(i%2!=0)
-                    {
-                        if(i==1)
-                            {
-                                a=1;
-                            }
-                        else
-                            {
-                                a=a*2;
-                            }
-                             printf("\n the number is : %d",a);
-                    }
-                else
-                    {
-                        if(i==2)
-                            {
-                                b=1;
-                            }
-                        else
-                            {
-                                b=b*3;
-                            }
-                             printf("\n the number is : %d",b);
-                    }
-            }
-             
-            
+    
+    //Checking of the nth term will be at even position or odd position
+    //Odd positions are powers of 2
+    //Even positions are powers of 3
+    if(n%2==0)
+    {
+        //nth position(if even) will be at n/2 position for sub GP-2
+        three(n/2);
+    }
+    else
+    {
+        //nth position(if odd) will be at (n/2 + 1) position for sub GP-1
+        two(n/2 + 1);
+    }
+    return 0;
 }
-
